38e3b2c1d4799175fe133183af8e31e1fe84e5a1
z-Fernando <prog.lffp@gmail.com>
2023-06-03 13:04:39 -0300

    adiciona corpo da página about (texto aleatorio) e links para home/contact. também cria o css necessário para funcionalidade desktop (pagina about).

--- react-portfolio/src/pages/About.jsx
+++ react-portfolio/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import '../stylesheets/about.css';
 import euJogando from '../img/me-cs.jpeg'
 import Header from "../components/Header";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class About extends React.Component {
   render() {
@@ -13,14 +13,31 @@ class About extends React.Component {
         <main>
           <section>
             <img src={ euJogando } alt="Autor do site em frente ao computador" />
-            <h1>Um pouco sobre a minha pessoa...</h1>
           </section>
           <section>
+            <h1>Um pouco sobre a minha pessoa...</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
           </section>
+          <section>
+            <h1>Um pouco sobre a minha pessoa...</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </section>
+          <section>
+            <img src={ euJogando } alt="Autor do site em frente ao computador" />
+          </section>
         </main>
+        <footer>
+          <Link to="/">
+            <div>Início</div>
+          </Link>
+          <Link to="/contact">
+            <div>Contato</div>
+          </Link>
+        </footer>
       </section>
     )
   }
