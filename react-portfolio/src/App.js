7fe6e186ebf43b8329c258af36a1a0f0b24598eb
z-Fernando <prog.lffp@gmail.com>
2023-06-11 13:01:23 -0300

    adiciona a pagina de projetos e atualiza o design da tela inicial

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -5,6 +5,7 @@ import Homepage from './pages/Homepage'
 import About from './pages/About';
 import Techs from './pages/Techs';
 import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 class App extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class App extends React.Component {
           <Route path="/about-me" component={ About } />
           <Route path="/techs" component={ Techs } />
           <Route path="/contact" component={ Contact } />
+          <Route path="/projects" component={ Projects } />
         </Switch>
       </Router>
     );
--- react-portfolio/src/pages/Homepage.jsx
+++ react-portfolio/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import developer from "../img/developing.png"
 
 class Homepage extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class Homepage extends React.Component {
         <main>
           <section className="picture">
             <img 
-              src="https://avatars.githubusercontent.com/u/112187309?v=4" 
+              src={ developer } 
               alt="Foto do criador do site" />
           </section>
           <section className="page-anchors">
